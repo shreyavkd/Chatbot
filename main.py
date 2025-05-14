d34a33b51adebc793caba9c07772ddc285a4e2c0
Shreya <shreyavkd@gmail.com>
2025-05-14 13:41:45 +0100

    Added API key discretely

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
-import openai
-from build_prompt import build_prompt
-
+from openai import OpenAI
 import os
-openai.api_key = os.getenv("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")
+from dotenv import load_dotenv
+
+#loading API key discretely
+load_dotenv()
+
+#initialising OpenAI client
+client= OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
 
 '''
 function to produce answer based on user query
@@ -10,20 +14,32 @@ function to produce answer based on user query
     output: response from gpt-4o-mini
 '''
 def answer_query(query):
-    messages = build_prompt(query)
+    messages= [
+                {"role": "system", 
+                 "content": "You are a helpful financial assistant."
+                 "If asked any non-financial questions, please decline politely."},
+                {"role": "user", "content": query}
+            ]
 
     try:
-        response = openai.ChatCompletion.create(model= "gpt-4o-mini", messages= messages)
+        response= client.chat.completions.create(model= "gpt-4o-mini", messages= messages)
         return response.choices[0].message["content"]
+    
+    except Exception as err:
+        return (f"Error:\n{err}\n")
 
 #initial message to user
 if __name__ == "__main__":
     print("Welcome to the Finance Chatbot!")
-    print("Please enter your finanial query.")
+    print("Please enter your finanial query. Type 'q' to quit.")
 
 #running chatbot until user quits
 while True:
     user_input= input("Q: ")
+
+    if user_input.lower() == ("q"):
+        exit()
+
     answer= answer_query(user_input)
     print(f"A: {answer}\n")
 
